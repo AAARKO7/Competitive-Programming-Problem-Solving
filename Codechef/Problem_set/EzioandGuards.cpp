588eeda61472e61cf9c48767440c4f73354dcf42
AAARKO7 <aadhikaryjs2@gmail.com>
2024-12-01 20:20:13 +0600

    yes

---
+++ Codechef/Problem_set/EzioandGuards.cpp
@@ -0,0 +1,41 @@
+/*
+    Solver: aaarko7
+    Date: 29 April,2023
+
+*/
+
+/*Ezio and Guards*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int x,y;
+
+    cin>>x>>y;
+
+    if(x>=y)
+    {
+        cout<<"YES"<<endl;
+    }
+
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
+
+int main()
+{
+    int t;
+
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Codechef/Problem_set/IncreaseIQ.cpp
@@ -0,0 +1,29 @@
+/*
+    Solver: aaarko7
+    Date: 29 April,2023
+
+*/
+
+/*Increase IQ*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    cin>>x;
+
+    if(x+7>170)
+    {
+        cout<<"Yes"<<endl;
+    }
+
+    else
+    {
+        cout<<"No"<<endl;
+    }
+
+    return 0;
+}
